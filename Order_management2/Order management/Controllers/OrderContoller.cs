217eca27adbe0e3458eb64642cf06cb29d187827
mariasabi <mariasabi16@gmail.com>
2024-06-06 18:41:05 +0530

    Order management with logging

---
+++ Order_management2/Order management/Controllers/OrderContoller.cs
@@ -0,0 +1,86 @@
+﻿using log4net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Order_management.Interfaces;
+using Order_management.Models;
+using Order_management.Service;
+
+namespace Order_management.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderContoller : ControllerBase
+    {
+        private readonly IOrder _order;
+        private static readonly ILog log = LogManager.GetLogger(typeof(OrderContoller));
+        public OrderContoller(IOrder orderservice)
+        {
+            _order=orderservice;
+        }
+        [HttpGet]
+        [Route("getItems")]
+        public async Task<ActionResult<Item>> GetItems()
+        {
+            var items=await _order.GetItems();
+            if (items == null)
+            {
+                log.Debug("No item found!");
+                return BadRequest("No item found!");
+            }
+            log.Info("Retrieved items");
+            return Ok(items);
+        }
+
+        [HttpGet]
+        [Route("getItem")]
+        public async Task<ActionResult<Item>> GetItem(int id)
+        {
+            var item = await _order.GetItem(id);
+            if (item == null)
+            {
+                log.Debug("No item found!");
+                return BadRequest("No item found!");
+            }
+            log.Info("Retrieved item");
+            return Ok(item);
+        }
+
+        [HttpPost]
+        [Route("addItem")]
+        public async Task<ActionResult<Item>> AddItem(Item request)
+        {
+            var item = await _order.AddItem(request);
+            // if (item==null)
+            //   return BadRequest("Such an item already exists!");
+            log.Info("Item added");
+            return Ok(item);
+        }
+
+        [HttpPut]
+        [Route("updateItem")]
+        public async Task<ActionResult<Item>> UpdateItem(Item request)
+        {
+            var item = await _order.UpdateItem(request);
+            if (item == null)
+            {
+                log.Debug("No item found!");
+                return BadRequest("No item found!");
+            }
+            log.Info("Item updated");
+            return Ok(item);
+        }
+        [HttpGet]
+        [Route("deleteItem")]
+        public async Task<ActionResult<Item>> DeleteItem(int id)
+        {
+            var item = await _order.DeleteItem(id);
+            if (item == null)
+            {
+                log.Debug("No item found!");
+                return BadRequest("No item found!");
+            }
+            log.Info("Item deleted");
+            return Ok(item);
+        }
+    }
+}
---
+++ Order_management2/Order management/Service/Order.cs
@@ -0,0 +1,72 @@
+﻿using log4net;
+using log4net.Config;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Order_management.Interfaces;
+using Order_management.Models;
+
+
+namespace Order_management.Service
+{
+    
+    public class Order:IOrder
+    {
+       
+
+        private readonly OrderManagementContext _context;
+        private static List<Item> items = new List<Item>();
+        public Order(OrderManagementContext context)
+        {
+            _context = context;
+            XmlConfigurator.Configure();
+        }
+        public Task<List<Item>> GetItems()
+        {
+            
+            return _context.Items.ToListAsync();
+        }
+        public async Task<Item> GetItem(int id)
+        {
+            var item =  _context.Items.FirstOrDefault(x => x.Id == id);
+            return item;
+        }
+        public async Task<Item> AddItem(Item request)
+        {
+           /* var existingItem = _context.Items.FirstOrDefault(x => x.Id == request.Id);
+            if (existingItem != null)
+            {
+                return null;
+            } */
+            _context.Items.Add(request);
+            _context.SaveChanges();
+            
+            return request;
+        }
+        public async Task<Item> UpdateItem(Item request)
+        {
+            var existingItem = _context.Items.FirstOrDefault(x => x.Id == request.Id);
+            if (existingItem == null)
+            {
+                return null;
+            }
+            existingItem.Quantity = request.Quantity;
+            existingItem.Name = request.Name;
+            existingItem.Type = request.Type;
+            _context.SaveChanges();
+            
+            return request;
+        }
+        public async Task<Item> DeleteItem(int id)
+        {
+            var item = _context.Items.FirstOrDefault(x => x.Id == id);
+            if (item == null)
+            {
+             return null;
+            }
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+           
+            return item;
+        }
+    }
+}
